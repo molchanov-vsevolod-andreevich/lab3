362047a1c8fa45bb22dc9e1fd30be9262bd96a43
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-21 07:46:32 +0300

    Scripted auto-commit on change (2019-10-21 07:46:32) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
     SparkConf conf = new SparkConf().setAppName("lab3");
-    
+    JavaSparkContext sc = new JavaSparkContext(conf);
 }
