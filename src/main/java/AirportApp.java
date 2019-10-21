f9c3724d1692ab0d36ca0db8a5afc43ba4a548b2
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-21 08:23:28 +0300

    Scripted auto-commit on change (2019-10-21 08:23:28) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -10,7 +10,7 @@ public class AirportApp {
 
         JavaRDD<String> flightsRDD = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airportsRDD = sc.textFile("L_AIRPORT_ID.csv");
-        JavaPairRDD<String, String> airportsInfo = airportsRDD.collectAsMap();
+        JavaPairRDD<String, String> splittedAirportsRDD = 
 
 //        JavaRDD<String> splittedFlightsInfo = flightsInfo.flatMap();
     }
