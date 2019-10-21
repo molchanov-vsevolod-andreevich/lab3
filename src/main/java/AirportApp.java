b41dc21877c8d5ccc1074f41bf19fde9f440a00c
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-21 07:55:58 +0300

    Scripted auto-commit on change (2019-10-21 07:55:58) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -3,14 +3,11 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
-    SparkConf conf = new SparkConf().setAppName("lab3");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-
-    JavaRDD<String> flightsInfo = sc.textFile("664600583_T_ONTIME_sample.csv");
-
-    private static final void
-
     public static void main(String[] args) {
-        initializeSpark();
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> flightsInfo = sc.textFile("664600583_T_ONTIME_sample.csv");
+        
     }
 }
