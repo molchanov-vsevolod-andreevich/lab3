581f0b0ff6978c9dc7f81c4cfdf24ed15d6e0c69
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-21 07:48:01 +0300

    Scripted auto-commit on change (2019-10-21 07:48:01) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,7 +1,10 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
     SparkConf conf = new SparkConf().setAppName("lab3");
     JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> distFile = sc.textFile(Hadoop "war-and-peace-1.txt");
 }
