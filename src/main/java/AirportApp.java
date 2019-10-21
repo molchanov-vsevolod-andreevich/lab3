7cda0212ca45b67a1f88727e0bea4816a96ba29f
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-21 13:35:23 +0300

    Scripted auto-commit on change (2019-10-21 13:35:23) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,3 +1,4 @@
+import org.apache.commons.csv.CSVParser;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -15,7 +16,7 @@ public class AirportApp {
         JavaRDD<String> airportsRDD = sc.textFile("L_AIRPORT_ID.csv");
 
 //        JavaRDD<String> splittedAirportsRDD = airportsRDD.flatMap(s -> Arrays.stream(s.substring(s.indexOf('\n')+1).replace("\"", "").replace(",", ";").split("\n")).iterator());
-        JavaRDD<String> splittedAirportsRDD = airportsRDD.flatMap(s -> Arrays.stream(s.split(",[\"]")).iterator());
+        JavaRDD<String> splittedAirportsRDD = airportsRDD.flatMap(s -> Arrays.stream(CSVParser.parse()).iterator());
 //        JavaPairRDD<String, String>
 //        System.out.println(splittedAirportsRDD.collect());
 //        List<String> l = splittedAirportsRDD.collect();
