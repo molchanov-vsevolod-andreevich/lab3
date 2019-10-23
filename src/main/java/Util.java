8b94bc384ec1ef4a17b665efa78fbcaa250240fe
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-23 12:53:46 +0300

    Scripted auto-commit on change (2019-10-23 12:53:46) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,8 +1,7 @@
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 
 public abstract class Util {
-    public static final parseCSVWithHeader(JavaRDD<String>) {
-
+    public static final JavaRDD<String> parseCSVWithHeader(JavaRDD<String> csvFile) {
+        
     }
 }
