d486a359b23e42556965e1cc64a7e5ea7a8e9474
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-23 12:56:41 +0300

    Scripted auto-commit on change (2019-10-23 12:56:41) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,6 +1,3 @@
-//import org.apache.commons.csv.CSVFormat;
-//import org.apache.commons.csv.CSVParser;
-//import org.apache.commons.csv.CSVRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -3,6 +3,6 @@ import org.apache.spark.api.java.JavaRDD;
 public abstract class Util {
     public static final JavaRDD<String> parseCSVWithHeader(JavaRDD<String> csvFile) {
         String header = csvFile.first();
-        
+
     }
 }
