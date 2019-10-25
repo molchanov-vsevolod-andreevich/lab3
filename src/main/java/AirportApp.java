f66b81563264820e9acaeca5e97c5229d9ae70da
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 17:48:08 +0300

    Scripted auto-commit on change (2019-10-25 17:48:08) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -19,7 +19,7 @@ public class AirportApp {
         Map<String, String> airportsCodesToNames = Util.parseAirports(airportsCSV);
         JavaPairRDD<Tuple2<String, String>, String> airportsPairsDelaysInfo = Util.parseFlights(flightsCSV);
 //        JavaPairRDD<Tuple2<String, String>, String> res = airportsPairsDelaysInfo.reduceByKey((a, b) -> a + " " + b);
-        JavaPairRDD<> = airportsPairsDelaysInfo.groupByKey();
+        JavaPairRDD<Tuple2<String, String>, Iterable<String>> res = airportsPairsDelaysInfo.groupByKey();
 
         System.out.println();
         System.out.println();
@@ -30,7 +30,7 @@ public class AirportApp {
 //        JavaPairRDD<String, String> airportsPairsDelaysInfo = Util.parseFlights(flightsCSV);
 //        Map<String, String> m = airportsPairsDelaysInfo.collectAsMap();
 //        airportsPairsDelaysInfo.foreach(v -> System.out.println(v._1() + " : " + v._2()));
-        Map<Tuple2<String, String>, String> m = res.collectAsMap();
+        Map<Tuple2<String, String>, Iterable<String>> m = res.collectAsMap();
         m.forEach((k, v) -> System.out.println(k + " => " + v));
         System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         System.out.println();
