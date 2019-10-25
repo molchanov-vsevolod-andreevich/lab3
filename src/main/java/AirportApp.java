dd4e5879369adf9f82c184e1a1af1307909e1222
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 17:47:20 +0300

    Scripted auto-commit on change (2019-10-25 17:47:20) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -18,8 +18,8 @@ public class AirportApp {
 
         Map<String, String> airportsCodesToNames = Util.parseAirports(airportsCSV);
         JavaPairRDD<Tuple2<String, String>, String> airportsPairsDelaysInfo = Util.parseFlights(flightsCSV);
-        JavaPairRDD<Tuple2<String, String>, String> res = airportsPairsDelaysInfo.reduceByKey((a, b) -> a + " " + b);
-
+//        JavaPairRDD<Tuple2<String, String>, String> res = airportsPairsDelaysInfo.reduceByKey((a, b) -> a + " " + b);
+        JavaPairRDD<> = airportsPairsDelaysInfo.groupByKey();
 
         System.out.println();
         System.out.println();
