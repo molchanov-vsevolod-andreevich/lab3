bd46d09f4922ccc72ab0f149e8386b5ce1e1a29f
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 16:34:14 +0300

    Scripted auto-commit on change (2019-10-25 16:34:14) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -29,7 +29,7 @@ public class AirportApp {
         System.out.println();
         System.out.println();
         System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        res.foreach();
+        res.foreach((k, v) -> );
         System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         System.out.println();
         System.out.println();
