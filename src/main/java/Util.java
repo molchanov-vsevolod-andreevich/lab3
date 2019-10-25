133ad9ef1bd94771aaaf6551fa041fcca0c37399
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 15:00:17 +0300

    Scripted auto-commit on change (2019-10-25 15:00:17) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -10,10 +10,6 @@ public abstract class Util {
         return csvFile.filter(line -> !line.equals(header));
     }
 
-    public static final Tuple2 getAirportsPair(String s) {
-
-    }
-
     public static final String[] parseCSVLineWithDelimiter(String line, String delimiter) {
         String[] records = line.split(delimiter);
         for (String record : records) {
@@ -25,7 +21,7 @@ public abstract class Util {
     public static final Map<String, String> parseAirports(JavaRDD<String> airportsCSV) {
         JavaRDD<String> airportsWithoutHeader = Util.removeHeader(airportsCSV);
         JavaPairRDD<String, String> airportsPairs = airportsWithoutHeader.mapToPair(s -> {
-            String[]  = Util.parseCSVLineWithDelimiter(s, Common.AIRPORTS_DELIMITER);
+            String[] records = Util.parseCSVLineWithDelimiter(s, Common.AIRPORTS_DELIMITER);
             return new Tuple2<>(records[0], records[1]);
         });
     }
