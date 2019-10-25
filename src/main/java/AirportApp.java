b861c80c676f4ca6a5f2fd7666fe8cad997e6ebe
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 15:20:39 +0300

    Scripted auto-commit on change (2019-10-25 15:20:39) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -16,7 +16,7 @@ public class AirportApp {
         JavaRDD<String> airportsCSV = sc.textFile("L_AIRPORT_ID.csv");
 
         Map<String, String> airportsCodesToNames = Util.parseAirports(airportsCSV);
-        
+        JavaPairRDD<>
 
         System.out.println();
         System.out.println();
