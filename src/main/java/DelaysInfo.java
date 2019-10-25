81d6fdbf053f0a356fb5322dbf7721dd1101da37
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 18:26:00 +0300

    Scripted auto-commit on change (2019-10-25 18:26:00) by gitwatch.sh

--- src/main/java/DelaysInfo.java
+++ src/main/java/DelaysInfo.java
@@ -18,6 +18,6 @@ public class DelaysInfo implements Serializable {
 
     @Override
     public String toString() {
-        return "maximal delay: " + maxDelay + " " + delayAndCancelPercentage + "%";
+        return "maximal delay: " + maxDelay + " percentage of delayed and canceled " + delayAndCancelPercentage + "%";
     }
 }
