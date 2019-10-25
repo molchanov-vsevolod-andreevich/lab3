2df59a08d5f7131ea8fd7da40da258f844e7995d
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 19:24:05 +0300

    Scripted auto-commit on change (2019-10-25 19:24:05) by gitwatch.sh

--- src/main/java/AirportsDelaysInfo.java
+++ src/main/java/AirportsDelaysInfo.java
@@ -1,5 +1,15 @@
 import java.io.Serializable;
 
 public class AirportsDelaysInfo implements Serializable {
-    private 
+    private String originAirportId;
+    private String originAirportName;
+
+    private String destAirportId;
+    private String destAirportName;
+
+    private DelaysInfo delays;
+
+    public AirportsDelaysInfo() {
+
+    }
 }
