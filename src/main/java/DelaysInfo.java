b545886654b09858be0d14a229e00411d3e67fad
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 18:23:06 +0300

    Scripted auto-commit on change (2019-10-25 18:23:06) by gitwatch.sh

--- src/main/java/DelaysInfo.java
+++ src/main/java/DelaysInfo.java
@@ -12,7 +12,7 @@ public class DelaysInfo implements Serializable {
         return delayAndCancelPercentage;
     }
 
-    public DelaysInfo() {
+    public DelaysInfo(String delaysInString) {
         
     }
 }
