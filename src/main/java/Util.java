400f8fa74a07669e700cee00fb454d3e5986b4d0
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 19:04:15 +0300

    Scripted auto-commit on change (2019-10-25 19:04:15) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -37,7 +37,7 @@ public abstract class Util {
             return new Tuple2<>(new Tuple2<>(records[Common.CSV_ORIGIN_AIRPORT_ID_INDEX], records[Common.CSV_DEST_AIRPORT_ID_INDEX]), records[Common.CSV_DELAY_INDEX]);
         }).groupByKey();
 
-        return airportsPairsDelaysInfo.map()
+        return airportsPairsDelaysInfo.mapToPair((k, v) -> );
     }
 
 //    public static final JavaPairRDD<Tuple2<String, String>, String> parseFlights(JavaRDD<String> flightsCSV) {
