11284cd84c498bd1c5c158ade356f611596cba9b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 14:30:11 +0300

    Scripted auto-commit on change (2019-10-25 14:30:11) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -5,7 +5,7 @@ public abstract class Util {
     public static final JavaRDD<String[]> parseCSVWithHeaderAndDelimiter(JavaRDD<String> csvFile, String delimiter) {
         String header = csvFile.first();
         JavaRDD<String> csvWithoutHeader = csvFile.filter(line -> !line.equals(header));
-        
+
     }
 
     public static final Tuple2 getAirportsPair(String s) {
