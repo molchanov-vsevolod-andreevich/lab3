325c61d6fe4a9cbc9e0dcd27ac73a3db8992d7ab
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 15:49:40 +0300

    Scripted auto-commit on change (2019-10-25 15:49:40) by gitwatch.sh

--- src/main/java/Common.java
+++ src/main/java/Common.java
@@ -6,4 +6,5 @@ public abstract class Common {
     public static final int CSV_AIRPORT_NAME_INDEX = 1;
     public static final int CSV_ORIGIN_AIRPORT_ID_INDEX = 11;
     public static final int CSV_DEST_AIRPORT_ID_INDEX = 14;
+    public static final int CSV_DELAY_INDEX = 18;
 }
