6b91fb0d95acdc7af4a32d5ce15088387435bf76
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 17:59:27 +0300

    Scripted auto-commit on change (2019-10-25 17:59:27) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -45,11 +45,11 @@ public class AirportApp {
     static class TupleComparator implements Comparator<Tuple2<String, String>>, Serializable {
         @Override
         public int compare(Tuple2<String, String> o1, Tuple2<String, String> o2) {
-            System.out.println();
             int res = o1._1().compareTo(o2._1());
             if (res == 0) {
                 res = o1._2().compareTo(o2._2());
             }
+            System.out.println("COMPARE: " + o1 + " : " + o2 + " => " + res);
             return res;
         }
     }
