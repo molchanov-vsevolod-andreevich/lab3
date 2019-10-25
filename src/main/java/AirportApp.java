30685fa35cd9f00b918c72c401c0e95a32cc3f0a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 17:52:14 +0300

    Scripted auto-commit on change (2019-10-25 17:52:14) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -4,7 +4,9 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -38,5 +40,14 @@ public class AirportApp {
         System.out.println();
         System.out.println();
         System.out.println();
+    }
+
+    class TupleComparator implements Comparator<Tuple2<Integer, Integer>>, Serializable {
+        @Override
+        public int compare(Tuple2<Integer, Integer> o1, Tuple2<Integer, Integer> o2) {
+            if (o1._1() == o2._1())
+                return o1._2() - o2._2();
+            return o1._1() - o2._1();
+        }
     }
 }
