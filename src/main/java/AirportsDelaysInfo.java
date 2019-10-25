26df8be2715f420f4d576011b209f2152b4d3e09
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-25 20:04:47 +0300

    Scripted auto-commit on change (2019-10-25 20:04:47) by gitwatch.sh

--- src/main/java/AirportsDelaysInfo.java
+++ src/main/java/AirportsDelaysInfo.java
@@ -40,6 +40,6 @@ public class AirportsDelaysInfo implements Serializable {
 
     @Override
     public String toString() {
-        return "FROM " + originAirportName + " (code " + originAirportID + ") \nTO " + destAirportName + " (code " + destAirportID + ")\n\t" + delays;
+        return "FROM: " + originAirportName + " (code " + originAirportID + ") \nTO: " + destAirportName + " (code " + destAirportID + ")\n\t" + delays;
     }
 }
